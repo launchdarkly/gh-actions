08b19b50cd5e0fd4fd9292ba106db158f13e198d
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2024-02-20 14:55:23 -0800

    Ensure errors stop script.

--- actions/release-secrets/s3-copy.sh
+++ actions/release-secrets/s3-copy.sh
@@ -1,3 +1,5 @@
+set -e
+
 # Use sed to remove potential whitespace around the '=' in the pairs.
 # Then split the string based on ',' to get an array of pairs.
 IFS=',' read -ra pairs <<< $(echo "${PATH_PAIRS}" | sed 's/[[:space:]]*=[[:space:]]*/=/g')
